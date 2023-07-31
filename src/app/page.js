b67c150a87c09af65090306d0bc3210b49b62e96
mayowasamuel <87167237+Mayowasamuel51@users.noreply.github.com>
2023-07-31 17:05:01 +0000

    added

--- src/app/page.js
+++ src/app/page.js
@@ -1,40 +1,11 @@
-'use client'
-import { Suspense, useEffect } from 'react'
-import { redirect, useRouter } from "next/navigation"
-import Navbar from "./components/Navbar"
-import { useSession, signIn } from 'next-auth/react'
-import PortSee from './PortSee'
-import ErrorBoundary from './ErrorBoundary'
+
+
 function Home() {
-  const session = useSession()
-  const router = useRouter()
-  if (process.env.NODE_ENV === "development") {
-    useEffect(() => {
-      if (session?.status === 'authenticated') {
-        // router('http://localhost:3000/dashboard')
-        redirect('http://localhost:3000/dashboard')
-      }
-    })
-  }
-  const handleSignIn = () => {
-    signIn('google', { callbackUrl: 'http://localhost:3000/dashboard' }); // Initiates the Google login flow
-  };
+  
 
   return (
     <>
-      <div>
-        {session?.status === "authenticated" ? <Navbar signIn={handleSignIn} /> : <Navbar signIn={handleSignIn} />}
-
-        <div className='container mt-4'>
-          <h2 className='text-center text-xl font-bold'>Developer's Portfolios</h2>
-          <ErrorBoundary>
-            <Suspense fallback={<h2>LOADING PLEASE WAIT............</h2>}>
-              <PortSee />
-            </Suspense>
-          </ErrorBoundary>
-        </div>
-
-      </div>
+     
     </>
   )
 }
