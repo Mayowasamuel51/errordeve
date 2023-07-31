9cffddd392e3a8f44fc507e7ca91004d657d7740
mayowasamuel <87167237+Mayowasamuel51@users.noreply.github.com>
2023-07-31 17:52:49 +0000

    added

--- src/app/devs/page.js
+++ src/app/devs/page.js
@@ -2,7 +2,7 @@ import { Suspense } from 'react'
 import LoadPort from "../LoadPort"
 import NetworkError from "../NetworkError"
 async function fetchports() {
-    const res = await fetch('http://localhost:3000/api/portfoillo')
+    const res = await fetch(`http://localhost:3000/api/portfoillo`)
     const data = await res.json()
     if (!res.ok) {
         return <NetworkError />
@@ -11,17 +11,15 @@ async function fetchports() {
 }
 
 const Dev = async () => {
-
     const portsee = await fetchports()
-
     return (
         <>
             <div>
                 <div className='container mt-4'>
                     <h2 className='text-center text-xl font-bold'>Developer's Portfolios</h2>
-                    <Suspense fallback={<h2>LOADING PLEASE WAIT............</h2>}>
+                    {/* <Suspense fallback={<h2>LOADING PLEASE WAIT............</h2>}> */}
                         <LoadPort port={portsee} />
-                    </Suspense>
+                    {/* </Suspense> */}
 
                 </div>
             </div>
