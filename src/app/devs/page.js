1ffd167441b0e69216b132d0c0a119135563ad74
mayowasamuel <87167237+Mayowasamuel51@users.noreply.github.com>
2023-08-03 10:56:29 +0000

    done we do

--- src/app/devs/page.js
+++ src/app/devs/page.js
@@ -1,50 +1,50 @@
-import { Suspense } from 'react'
+import React, { Suspense } from 'react'
 import LoadPort from '../LoadPort';
 import NetworkError from "../NetworkError"
 
 async function Ports() {
-  const response = await fetch(`http://localhost:3000/api/port`,{
-    next:{ revalidate: 10 } 
+  const response = await fetch(`https://errordeve.vercel.app/api/port`, {
+    next: { revalidate: 10 }
   });
   try {
     if (!response.ok) {
-        if (response.status === 404) {
-            // return <NetworkError />
-            throw new Error('Resource not found');
-        } else if (response.status === 500) {
-            // return <NetworkError />
-            throw new Error('Internal server error');
-        } else {
-            // return <NetworkError />
-            throw new Error('Unknown server error');
-        }
+      if (response.status === 404) {
+        // return <NetworkError />
+        throw new Error('Resource not found');
+      } else if (response.status === 500) {
+        // return <NetworkError />
+        throw new Error('Internal server error');
+      } else {
+        // return <NetworkError />
+        throw new Error('Unknown server error');
+      }
     }
     const data = await response.json()
     return data.data
-} catch (err) {
+  } catch (err) {
     return <NetworkError />
+  }
+
+
 }
 
-  
-}
 
-
-async function Dev(props) {
-  // const portsee = await Ports()
+async function Dev() {
+  const portsee = await Ports()
   // console.log(portsee)
   return (
-   
-      <div>
-        <div className='container mt-4'>
-          <h2 className='text-center text-xl font-bold'>Developer's Portfolios</h2>
 
-          {/* <Suspense fallback={<h2>Loading..............</h2>}>
-            <LoadPort port={portsee} />
-          </Suspense> */}
+    <div>
+      <div className='container mt-4'>
+        <h2 className='text-center text-xl font-bold'>Developer's Portfolios</h2>
 
-        </div>
+        <Suspense fallback={<h2>Loading..............</h2>}>
+          <LoadPort port={portsee} />
+        </Suspense>
+
       </div>
-   
+    </div>
+
   )
 }
 
